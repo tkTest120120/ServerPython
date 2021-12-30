6db15d9772bef674e798a1097ad3fb061b9a82d7
huy2012 <huynn@reactplus.jp>
2021-12-30 13:22:07 +0700

    update

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 # pip freeze > requirements.txt export library to file txt
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
-@app.get("/")
-def main():
-    return "Welcome!"
+
+@app.route("/")
+def home():
+    return "Hello, World!"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True)
